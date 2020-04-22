893a54ee6a10ab490eb1e79bab30c64f7f296e63
Ivan Lesar <ivan.lesar.pmf@gmail.com>
2020-04-22 13:42:42 +0200

    Registration working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {Client, connect} from 'mqtt';
 import * as https from 'https';
 import axios, {AxiosResponse} from 'axios';
+
 const querystring = require('querystring');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
@@ -17,7 +18,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
                     console.error(error);
                 }
             });
-            client.subscribe('15ledstrip', function (error: Error) {
+            client.subscribe('5ledstrip/config', function (error: Error) {
                 if (!error) {
                     console.error(error);
                 }
@@ -27,10 +28,13 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
         client.on('message', (topic, message) => {
             if (topic === "configuration") {
+                const jsonPayload = JSON.parse(message.toString());
+                console.log(jsonPayload);
                 axios.post(
                     'http://192.168.31.248:8000/api/mqtt/configuration',
                     querystring.stringify({
-                        deviceId: message.toString(),
+                        deviceId: jsonPayload.deviceId,
+                        configuration: jsonPayload.configs,
                     }),
                     {
                         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
@@ -39,7 +43,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
                     console.log(response.data);
                 }).catch((error) => {
                     console.error('ERROR');
-                    console.error(error);
+                    console.error(error.response);
                 });
             }
         });
@@ -47,20 +51,38 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
         console.error(e);
     }
 
-    axios.post(
-        'http://192.168.31.248:8000/api/mqtt/configuration',
-        querystring.stringify({
-            deviceId: '15ledstrip',
-        }),
-        {
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        }
-    ).then((response: AxiosResponse) => {
-        console.log(response.data);
-    }).catch((error) => {
-        console.error('ERROR');
-        console.error(error);
-    });
 
-
+    // const jsonPayload = {
+    //     deviceId: '5ledstrip',
+    //     configs: [{r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'},
+    //         {r: 255, g: 255, b: 255, type: 'color'}],
+    // };
+    // axios.post(
+    //     'http://192.168.31.248:8000/api/mqtt/configuration',
+    //     querystring.stringify({
+    //         deviceId: jsonPayload.deviceId,
+    //         configuration: JSON.stringify(jsonPayload.configs),
+    //     }),
+    //     {
+    //         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    //     }
+    // ).then((response: AxiosResponse) => {
+    //     console.log(response.data);
+    // }).catch((error) => {
+    //     console.error('ERROR');
+    //     console.error(error.response);
+    // });
 })();
